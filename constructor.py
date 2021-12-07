3357c7b384e5be5b5b5dc622d8ee3576969ef93f
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2021-12-07 10:18:16 +0530

    constructor

---
+++ constructor.py
@@ -0,0 +1,13 @@
+#contructor
+class Mobile:
+    #Constructor without parameter
+    def __init__(self):
+        self.model='ReadMe X'
+
+    def show_model(self):
+        print("Model:",self.model)
+
+realme=Mobile()
+realme.show_model()
+
+        
