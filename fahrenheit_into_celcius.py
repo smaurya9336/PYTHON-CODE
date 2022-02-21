981d90d4c0b9a9e60d4a305f7f753622ee40c682
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2022-02-21 11:25:46 +0530

    fahranheit in to celcius

---
+++ fahrenheit_into_celcius.py
@@ -0,0 +1,4 @@
+#write a program to convert fahrenheit in to celcius
+temprature=float(input("Please enter temprature in fahrenheit:"))
+celcius=(temprature -32)*5/9
+print("Temrature in celcius:",celcius)
